bbca1db267bc989033361a56f8df03e0683cb313
Shivam <shivam.gup82@gmail.com>
2021-05-15 17:25:03 +0530

    added test

--- unit_test.py
+++ unit_test.py
@@ -3,4 +3,4 @@ from main import conn, conn_forces, conn_info
 def test_connections():
     assert conn!=None
     assert conn_info!=None
-    assert conn_forces!=None
+    assert conn_forces!=None   
